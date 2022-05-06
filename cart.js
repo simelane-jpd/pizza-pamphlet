a68355528d35594abbcb79976360c189d0bdbec5
simelane-jpd <201575227@student.uj.ac.za>
2022-05-06 16:50:10 +0200

    fixed add button on cart

---
+++ cart.js
@@ -0,0 +1,76 @@
+console.log('it is working')
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzas', () => ({
+        smallCount: 0,
+        medCount: 0,
+        largeCount: 0,
+        smallPrice: 49.99,
+        medPrice: 89.99,
+        largePrice: 129.99,
+        tendered: '',
+        addSmall(){
+            this.smallCount++
+        },
+        addMed(){
+            this.medCount++
+        },
+        addLarge(){
+            this.largeCount++
+        },
+        removeSmall(){
+            this.smallCount--
+        },
+
+        removeMed(){
+            this.medCount--
+        },
+
+        removeLarge(){
+            this.largeCount--
+        },
+
+
+        getSmallTotal(){
+            let total = this.smallCount * this.smallPrice
+            return total
+        },
+        getMedTotal(){
+            let total = this.medCount * this.medPrice
+            return total
+        },
+        getLargeTotal(){
+            let total = this.largeCount * this.largePrice
+            return total
+        },
+
+        getTotal(){
+            let total = this.getSmallTotal() + this.getMedTotal() + this.getLargeTotal()
+            return total
+        },
+
+        feedbackText(){
+            if(this.getTotal() > parseFloat(this.tendered)){
+                //const change=this.getTotal - parseFloat
+               // message.innerHTML = "Enjoy your Pizza! your change is R" + change.toFixed(2);
+                return 'Funds too low'
+            }else if(this.getTotal() <= parseFloat(this.tendered)){
+                return 'Funds are sufficient'
+           
+            }
+        },
+
+        checkout(){
+            if(this.feedbackText() == 'Funds too low'){
+                window.alert('Declined! Insufficient funds.')
+                
+            }else{
+                
+                this.smallCount = 0
+                this.medCount = 0
+                this.largeCount = 0
+                window.alert("Enjoy your Pizza!")
+            }
+            
+        }
+    }))
+})
